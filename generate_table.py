7c12e3d3fc4e09658b0ad7dc7a1fbb80e6ec80b8
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-04-01 16:20:13 -0700

    Make sure to add the apple id

--- generate_table.py
+++ generate_table.py
@@ -2,6 +2,7 @@ import json
 import xml.etree.ElementTree as etree
 
 table = etree.Element("table", attrib={
+    "id": "apple",
     "class": "tablesorter",
     "cellspacing": "1",
     "cellpadding": "0",
